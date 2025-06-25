f9aa9312077e8e8c4045a3f70545267c57d8d6cd
Branndon-Schneyder-Suarez-Jimenez <branndon255225@gmail.com>
2025-06-25 05:50:45 -0500

    docs: pagina web
    Sin la parte de Andrea

---
+++ 3. Diseño Web-REPOSITORIO/PAGINA_WEB/0.INICIO_SESION/acceso_denegado.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso Denegado</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Acceso Denegado</h1>
+        <p>No tienes permisos para acceder a esta página.</p>
+        <a href="index.php">Volver al inicio</a>
+    </div>
+</body>
+</html>
